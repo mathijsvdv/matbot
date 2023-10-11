cf71c554e7a15d8866dd677139b2c958d9d46e81
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2023-10-11 11:22:52 +0200

    Add constants to make the Chainlit app lightweight

--- app.py
+++ app.py
@@ -1,45 +1,10 @@
 import chainlit as cl
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import RetrievalQA
-from langchain.embeddings.ollama import OllamaEmbeddings
-from langchain.llms import Ollama, OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.vectorstores import Chroma
 
-llms = {
-    "openai": OpenAI(temperature=0),
-    "mistral": Ollama(
-        model="mistral", verbose=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
-    ),
-}
-
-persist_directory = "./docs/chroma"
-embedding = OllamaEmbeddings(model="mistral")
-vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
-
-template = """You are a Retrieval-Augmented Generation chatbot that answers questions on
-documents provided to you. Act as an expert in the subject matter of the document
-discussed. If a question is not relevant for the document or if it cannot be answered
-using the information of the document, please do not answer the question and politely provide
-the reason.
-Document: {context}
-
-Question: {question}
-Helpful Answer:"""
+from constants import llm_chain
 
 
 @cl.on_chat_start
 def main():
-    # Instantiate the chain for that user session
-    prompt = PromptTemplate.from_template(template)
-    llm_chain = RetrievalQA.from_chain_type(
-        llm=llms["mistral"],
-        retriever=vectordb.as_retriever(),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": prompt, "verbose": True},
-    )
-
     # Store the chain in the user session
     cl.user_session.set("llm_chain", llm_chain)
 
@@ -47,7 +12,7 @@ def main():
 @cl.on_message
 async def main(message: str):
     # Retrieve the chain from the user session
-    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+    llm_chain = cl.user_session.get("llm_chain")  # type: RetrievalQA
 
     try:
         # Call the chain asynchronously
---
+++ constants.py
@@ -0,0 +1,37 @@
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import RetrievalQA
+from langchain.embeddings.ollama import OllamaEmbeddings
+from langchain.llms import Ollama, OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores.chroma import Chroma
+
+llms = {
+    "openai": OpenAI(temperature=0),
+    "mistral": Ollama(
+        model="mistral", verbose=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+    ),
+}
+
+persist_directory = "./docs/chroma"
+embedding = OllamaEmbeddings(model="mistral")
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+template = """You are a Retrieval-Augmented Generation chatbot that answers questions on
+documents provided to you. Act as an expert in the subject matter of the document
+discussed. If a question is not relevant for the document or if it cannot be answered
+using the information of the document, please do not answer the question and politely provide
+the reason.
+Document: {context}
+
+Question: {question}
+Helpful Answer:"""
+
+prompt = PromptTemplate.from_template(template)
+
+llm_chain = RetrievalQA.from_chain_type(
+    llm=llms["mistral"],
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": prompt, "verbose": True},
+)
